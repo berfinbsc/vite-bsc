7f7f8a1e4bfcac63dd85f0e221438eda274271ff
berfin7 <berfinbasci4701@gmail.com>
2023-12-13 02:55:33 +0300

    otomatik yönlendirme

--- src/Requests.jsx
+++ src/Requests.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { useParams ,Navigate, useNavigate} from 'react-router-dom' 
+import { useParams } from 'react-router-dom' 
 import axios from 'axios';
 
 const Requests = () => {
-  const navigate = useNavigate();
 
   const {link}= useParams()
-const [pagebsc,setPage]= useState("");
 
 useEffect (()=>{
 axios.get(`https://zetacoder.rocks/darvinapi/darvinberfin/${link}`)
 .then(response=>{
   
-  setPage(response.data.longlink);
   console.log(response.data);
+  return response.data.longlink;
+})
+.then(url=>{
+  window.location.replace(url);
 })
 .catch(error=>{
   console.log(error);
@@ -22,20 +23,19 @@ axios.get(`https://zetacoder.rocks/darvinapi/darvinberfin/${link}`)
 
 },[link])
 
-useEffect(() => {
-  if (link && pagebsc) {
-    const url = new URL(pagebsc);
-    navigate(url, { replace: true, target: "_blank" });
-  }
-}, [link,pagebsc]);
 
 
   return (
     <div>
-     <a href={pagebsc}>{pagebsc}</a>
 
     </div> 
   )
 
   }
 export default Requests;
+
+
+
+
+
+
